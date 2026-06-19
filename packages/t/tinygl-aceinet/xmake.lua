508ae9a0b998d5c6dc23371a5f34a725153596cb
aceinetx <mouselin82@gmail.com>
2026-06-19 07:27:08 +0000

    remove link

--- packages/t/tinygl-aceinet/xmake.lua
+++ packages/t/tinygl-aceinet/xmake.lua
@@ -1,6 +1,6 @@
 package("tinygl-aceinet")
 	set_description("A portable, software-only OpenGL 1.1 rasterizer in C99 (aceinet's fork)")
-	add_urls("https://github.com/aceinetx/tinygl/archive/$(version).zip", "https://github.com/aceinetx/tinygl.git")
+	add_urls("https://github.com/aceinetx/tinygl/archive/$(version).zip")
 
 	add_versions("afdbeb74e5dfe8e3e9031835fbdac059c2aa9a03", "8cba36feafb06e68bb334ce4f633d802e17d30f2f829aeab1f14b96c1d41d718")
 
